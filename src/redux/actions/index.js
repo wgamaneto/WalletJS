d332c4e2a97e940d35baacc72d3c6443b32e994e
washington gama <wgamaneto16@gmail.com>
2022-09-29 16:03:09 -0300

    terminando redux, acho

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,3 @@
 // Coloque aqui suas actions
+export const USER_EMAIL = 'USER_EMAIL';
+export const USER_WALLET = 'USER_WALLET';
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,1 +1,19 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { USER_WALLET } from '../actions';
+
+const INITIAL_STATE = {
+  wallet: '',
+};
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case USER_WALLET:
+    return {
+      ...state,
+      wallet: action.wallet,
+    };
+  default:
+    return state;
+  }
+};
+
+export default wallet;
