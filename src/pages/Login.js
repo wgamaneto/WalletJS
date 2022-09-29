166d0af3739901b4b2b4d41fa814cd7dfd8d3929
washington gama <wgamaneto16@gmail.com>
2022-09-29 18:14:35 -0300

    req 1?????????

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,69 @@
 import React from 'react';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    disableBtn: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.state({ [name]: value }, () => this.handleVerify());
+  };
+
+  handleVerify = () => {
+    const { email, password } = this.state;
+    const number = 5;
+    const passwordVerify = password.length > number;
+    const emailRgx = /\S+@\S+\.\S+/;
+    const emailVerify = emailRgx.test(email);
+    if (passwordVerify && emailVerify) {
+      this.setState({ disableBtn: false });
+    } else {
+      this.setState({ disableBtn: true });
+    }
+  };
+
+  handleClick = () => {
+    const { history, dispatch } = this.props;
+    const { email } = this.state;
+    dispatch(emailHandler(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <input
+          data-testid="email-input"
+          name="email"
+          type="email"
+          onChange={ this.handleChange }
+        />
+        <input
+          data-testid="password-input"
+          name="password"
+          type="password"
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          disabled={ disableBtn }
+          onClick={ this.handleClick }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect()(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,3 +1,8 @@
 // Coloque aqui suas actions
 export const USER_EMAIL = 'USER_EMAIL';
 export const USER_WALLET = 'USER_WALLET';
+
+export const getEmail = (email) => ({
+  type: USER_EMAIL,
+  payload: email,
+});
