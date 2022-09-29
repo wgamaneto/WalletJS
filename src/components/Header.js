12ffef3ac9d0b3255d847c83648991ea6e3628e4
washington gama <wgamaneto16@gmail.com>
2022-09-29 15:26:58 -0300

    requisito 1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <>
+        <div>Header</div>
+        <>
+          <div data-testid="email-field">
+            { email }
+          </div>
+          <div data-testid="total-field">
+            { 0 }
+          </div>
+          <div data-testid="header-currency-field">
+            BRL
+          </div>
+        </>
+      </>
     );
   }
 }
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
 
-export default Header;
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -9,10 +12,13 @@ ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
     <BrowserRouter>
-      <App />
+      <Provider store={ store }>
+        <App />
+      </Provider>
     </BrowserRouter>,
   );
 
+// ?????
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
